7272f92be22b39ba1b6617c89c3b7fbed8f883bb
ayush5718 <ayushbhardwaj9504@gmail.com>
2024-05-09 00:02:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Background from "./components/Background";
+
+function App() {
+  return (
+    <>
+      <div className="relative rw-full h-screen bg-zinc-800">
+        <Background />
+      </div>
+    </>
+  );
+}
+
+export default App;
